78c40e5348035da56e152cb01c1a139dbea5f8f3
uri983 <urivelasquez@gmail.com>
2023-04-19 17:42:01 -0500

    package

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+console.log('Ejecutando')
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -43,5 +43,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
